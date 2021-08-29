e4c41465086cfeff184a72446ebe70ee3fdd208c
RojosiGrammin <89713624+RojosiGrammin@users.noreply.github.com>
2021-08-28 21:23:25 -0500

    Update game.js
    add photos
    add takephotos
    add makepuzzle
    minor fixes

--- js/game.js
+++ js/game.js
@@ -3,7 +3,12 @@
     takePhotoButton = document.querySelector('.takePhoto');
 
   // New Game-Level Variables
-  let constraints, imageCapture, mediaStream, video;
+  let constraints, imageCapture, mediaStream, video, numCol = 3, numRow = 3;
+  let puzzlePieces = numCol * numRow;
+  let pieces = puzzlePieces - 1;
+  let imagePieces = new Array(puzzlePieces);
+  let puzzle = [...imagePieces.keys()].map(String);
+  let markers = document.querySelectorAll('a-marker');
 
   cost init = () => {
     video = document.querySelector('video');
@@ -47,6 +52,41 @@
 
   // Take the picture
   const getPicture = () => {
-
+    imageCapture.takePhoto()
+      .then((img) => {
+        image.src = URL.createObjectURL(img);
+        image.addEventListener('load', () => createImagePieces(image));
+        setInterval(() => checkDistance(), 1000);
+        console.log(puzzle);
+      })
+      .catch((error) => {console.log('takePhoto() error', error)});
   };
+
+  const createImagePieces = (image) => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const pieceWidth = image.width / numCol;
+    const pieceHeight = image.height / numRow;
+    for (lex x = 0; x < numCol; x++) {
+      for (let y = 0; y < numRow; y++) {
+        console.log(x, y);
+        // console output: 0,0 | 0,1 | 0,2 | 1,0 | etc.
+        ctx.drawImage(image, x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight, 0, 0, canvas.width, canvas.height);
+        imagePieces[8 - pieces] = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        console.log(imagePieces);
+        pieces = pieces - 3;
+        if (pieces < 0) {
+          pieces = (puzzlePieces - 1) + pieces;
+        }
+      }
+    }
+    markers.forEach((marker, index) => {
+      const aImg = new document.createElement('a-image');
+      aImg.setAttribute('rotation', '-90, 0, 0');
+      aImg.setAttribute('position', '0, 0, 0');
+      aImg.setAttribute('src', imagePieces[puzzle[index]]);
+      marker.appendChild(aImg);
+    })
+  };
+  const checkDistance = () => {};
 }
